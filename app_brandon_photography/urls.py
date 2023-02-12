1ca838c5769b8aea78dad15914114d4ab4488a31
Adam Nagy <nagada88@gmail.com>
2023-02-12 12:06:37 +0100

    refactor

--- app_brandon_photography/urls.py
+++ app_brandon_photography/urls.py
@@ -5,9 +5,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    re_path(r'^$', views.gallery, name='gallery'),
-    re_path(r'foto', views.foto, name='foto'),
+    re_path(r'^$', views.intro, name='intro'),
+    re_path(r'intro', views.intro, name='intro'),
     re_path(r'gallery', views.gallery, name='gallery'),
+    re_path(r'kiskedvenc', views.kiskedvenc, name='kiskedvenc'),
+    path(r'eskuvo.html', views.eskuvo, name='eskuvo'),
+    re_path(r'termek', views.termek, name='termek'),
+    re_path(r'portre', views.portre, name='portre'),
     re_path(r'video', views.video, name='video'),
-    re_path(r'bio', views.bio, name='bio'),
+    re_path(r'marketing', views.marketing, name='marketing'),
     re_path(r'kapcsolat', views.kapcsolat, name='kapcsolat'),]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app_brandon_photography/views.py
+++ app_brandon_photography/views.py
@@ -5,42 +5,52 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 
 # Create your views here.
+    
+def kiskedvenc(request):
+    pictures = Photos.objects.filter(category__name="kiskedvenc")
+    return render(request, 'kiskedvenc.html',  {'pictures': pictures})
 
-def bio(request):
-    return render(request, 'bio.html', {})
+def eskuvo(request):
+    pictures = Photos.objects.filter(category__name="esküvő")
+    return render(request, 'eskuvo.html',  {'pictures': pictures})
+    
+def termek(request):
+    pictures = Photos.objects.filter(category__name="termékfotó")
+    return render(request, 'termek.html',  {'pictures': pictures})
 
+def portre(request):
+    pictures = Photos.objects.filter(category__name="portré")
+    return render(request, 'portre.html',  {'pictures': pictures})
+    
 def video(request):
-    return render(request, 'video.html', {})
-
-def foto(request):
+    return render(request, 'video.html')
+    
+def marketing(request):
+    return render(request, 'marketing.html')
+        
+def intro(request):
     categories = PhotoCategory.objects.all().order_by('priority')
-    # for category in categories:
-    #     category.get_category_cover()
-    return render(request, 'foto.html', {'categories': categories})
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            body = {
+                'name': form.cleaned_data['name'],
+                'email_address': form.cleaned_data['email_address'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
 
-def gallery(request):
+            try:
+                send_mail(subject, message,  body['email_address'], [body['email_address']])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect("gallery.html")
 
-    category_id = request.GET.get('gallery_id')
-    if category_id:
-        pictures = Photos.objects.filter(category=category_id)
-    else:
-        pictures = Photos.objects.filter(main_site_visibility=True)
-
-    pictures = pictures.order_by('priority')
-    picture_quantity = len(pictures)
-    breakpnumber = round(picture_quantity/3.)
-    breakpremaining = picture_quantity%3
-    bplist = [0]
-    print(pictures[0].photo_tumb)
-    if breakpremaining in [0,2]:
-        bplist.append(breakpnumber)
-        bplist.append(2*breakpnumber)
-    elif breakpremaining in [1]:
-        bplist.append(breakpnumber+1)
-        bplist.append(2*breakpnumber+1)
-
-
-    return render(request, 'gallery.html', {'pictures': pictures, 'breakpnumber': breakpnumber, 'bplist': bplist})
+    form = ContactForm()
+    
+    return render(request, 'intro.html', {'categories': categories, 'form': form})
 
 def kapcsolat(request):
     if request.method == 'POST':
@@ -62,3 +72,30 @@ def kapcsolat(request):
 
     form = ContactForm()
     return render(request, "kapcsolat.html", {'form': form})
+    
+
+def gallery(request):
+    
+    category_id = request.GET.get('gallery_id')
+    if category_id:
+        pictures = Photos.objects.filter(category=category_id)
+        category = PhotoCategory.objects.filter(pk=category_id)
+    else:
+        pictures = Photos.objects.filter(main_site_visibility=True)
+    
+    print(category)
+    pictures = pictures.order_by('priority')
+    picture_quantity = len(pictures)
+    breakpnumber = round(picture_quantity/3.)
+    breakpremaining = picture_quantity%3
+    bplist = [0]
+    print(pictures[0].photo_tumb)
+    if breakpremaining in [0,2]:
+        bplist.append(breakpnumber)
+        bplist.append(2*breakpnumber)
+    elif breakpremaining in [1]:
+        bplist.append(breakpnumber+1)
+        bplist.append(2*breakpnumber+1)
+
+
+    return render(request, 'gallery.html', {'pictures': pictures, 'breakpnumber': breakpnumber, 'bplist': bplist, 'category': category})
