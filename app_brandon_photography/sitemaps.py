1da761b5f3204b191aeb0edcd86e6c4accf716b5
Adam Nagy <nagada88@gmail.com>
2023-04-09 14:47:30 +0200

    blog lehetoseg

--- app_brandon_photography/sitemaps.py
+++ app_brandon_photography/sitemaps.py
@@ -1,5 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from .models import BlogPost
 
 class StaticSitemap(Sitemap):
     changefreq = "yearly"
@@ -11,3 +12,15 @@ class StaticSitemap(Sitemap):
 
     def location(self, item):
         return reverse(item)
+        
+
+class BlogSitemap(Sitemap):
+    changefreq = "never"
+    priority = 0.6
+    protocol = 'https'
+    
+    def items(self):
+        return BlogPost.objects.all()
+
+    def lastmod(self, obj):
+        return obj.updated_at
